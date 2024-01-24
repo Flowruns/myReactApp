19ff5280a9678fbce5832c97c912942868f70fcd
EKATERINA_ZAKHAROVA <93439877+Flowruns@users.noreply.github.com>
2024-01-24 12:31:39 +0700

    Sorting posts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   //const [likes, setLikes] = useState(8)
@@ -17,17 +18,22 @@ function App() {
   // Состояние с массивом постов
   // в useState передаем массив объектов
   const [posts,setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'Description'},
-    {id: 2, title: 'JavaScript 2', body: 'Description'},
-    {id: 3, title: 'JavaScript 3', body: 'Description'}
+    {id: 1, title: 'JavaScript', body: 'Первый пост'},
+    {id: 2, title: 'JavaScript 2', body: '2 пост'},
+    {id: 3, title: 'JavaScript 3', body: 'Третий пост'}
   ])
-    
+    const [selectedSort, setSelectedSort] = useState('')
     const createPost = (newPost) => {
         setPosts([...posts, newPost])  
     }
     // Получаем Post из дочернего элемента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))  
+    }
+    
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
     }
 
   /*const [posts2,setPosts2] = useState([
@@ -110,6 +116,18 @@ function App() {
       
     <div className="App">
         <PostForm create = {createPost}/>
+        <hr style={{margin: '15px 0'}}/>
+        <div>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Сортировка"
+                options={[
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По описанию'}
+                ]}
+            />
+        </div>
         {posts.length !== 0
             ? 
             <PostList remove = {removePost} posts = {posts} title = {"Посты про JS"}/>
@@ -118,10 +136,8 @@ function App() {
                 Посты не найдены!
             </h1>
         }
-         
     </div>
   );
 }
-
 export default App;
-// 51:36
+
